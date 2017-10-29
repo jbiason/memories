d9cf070e6d84b6b10738b0facb2d87ce085c6f9c
Julio Biason <julio.biason@gmail.com>
2017-10-29 17:08:03 -0200

    force login

--- memories/defaults.py
+++ memories/defaults.py
@@ -5,5 +5,6 @@
 import os
 import os.path
 
-DEBUG=True
-STORAGE=os.path.join(os.getcwd(), 'entries')
+DEBUG = True
+STORAGE = os.path.join(os.getcwd(), 'entries')
+LOGIN_INFO = 'd2abaa37a7c3db1137d385e1d8c15fd2'
--- memories/main.py
+++ memories/main.py
@@ -4,14 +4,36 @@
 """A memory collection app."""
 
 from flask import Flask
+from flask import redirect
+from flask import request
+from flask import session
+from flask import url_for
 
-app = Flask(__name__)
+app = Flask(__name__)                           # pylint:disable=invalid-name
 
 app.config.from_object('memories.defaults')
 app.config.from_envvar('MEMORIES_SETTINGS', silent=True)
+
+
+@app.before_request
+def check_login():
+    """Check if there is a login information in the session; if there isn't,
+    redirects to the login page.
+    """
+    app.logger.debug('Endpoint: %s', request.endpoint)
+    if (('login' not in session or
+         session['login'] != app.config['LOGIN_INFO']) and
+            request.endpoint != 'login'):
+        return redirect(url_for('login'))
 
 
 @app.route('/')
 def index():
     """The main page."""
     return 'Hello world'
+
+
+@app.route('/login')
+def login():
+    """Ask for login information."""
+    return 'Login'
