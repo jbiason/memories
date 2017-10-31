1a5a3cfde18c648427ed8f11e8f85678e4c2d30e
Julio Biason <julio.biason@azion.com>
2017-10-31 12:53:42 -0200

    make the login validation

--- memories/defaults.py
+++ memories/defaults.py
@@ -8,3 +8,4 @@ import os.path
 DEBUG = True
 STORAGE = os.path.join(os.getcwd(), 'entries')
 LOGIN_INFO = 'd2abaa37a7c3db1137d385e1d8c15fd2'
+SECRET_KEY = 'twisiswewysecwet'
--- memories/main.py
+++ memories/main.py
@@ -4,8 +4,10 @@
 """A memory collection app."""
 
 import hashlib
+import os
 
 from flask import Flask
+from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
@@ -23,38 +25,41 @@ def check_login():
     """Check if there is a login information in the session; if there isn't,
     redirects to the login page.
     """
-    app.logger.debug('Endpoint: %s', request.endpoint)
-    if (('login' not in session or
-         session['login'] != app.config['LOGIN_INFO']) and
-            request.endpoint != 'login'):
-        return redirect(url_for('login'))
+    app.logger.debug('Endpoint: %s, path: %s',
+                     request.endpoint, request.full_path)
+    if 'login' not in session:
+        if not request.full_path.startswith(url_for('login')):
+            return redirect(url_for('login'))
 
 
 @app.route('/')
 def index():
     """The main page."""
-    return 'Hello world'
+    content = os.listdir(app.config['STORAGE'])
+    return render_template('index.html', dates=content)
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """Ask for login information."""
-    return render_template('login.html')
-
-
-@app.route('/login', methods=['POST'])
-def do_login():
-    """Check the login information."""
-    if 'username' not in request.values or 'password' not in request.values:
-        # XXX add missing fields error
+    if request.method == 'GET':
         return render_template('login.html')
+    else:
+        if ('username' not in request.values or
+                'password' not in request.values or
+                not request.values['username'] or
+                not request.values['password']):
+            app.logger.debug('Missing values')
+            flash('Missing values', 'alert')
+            return render_template('login.html')
 
-    digest = hashlib.md5('{}:{}'.format(
-        request.values['username'],
-        request.values['password'])).hexdigest()
-    if digest != app.config['LOGIN_INFO']:
-        # XXX add wrong login error
-        return render_template('login.html')
+        digest = hashlib.md5(u'{}:{}'.format(
+            request.values['username'],
+            request.values['password']).encode('utf-8')).hexdigest()
+        if digest != app.config['LOGIN_INFO']:
+            app.logger.debug('login info no match')
+            flash('Invalid login credentials', 'alert')
+            return render_template('login.html')
 
-    session['login'] = digest
-    return redirect(url_for('index'))
+        session['login'] = digest
+        return redirect(url_for('index'))
